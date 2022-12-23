1e6e29d95def989a6155caf8526501105dfc9d8f
Lwazi Mabota <greninja71@gmail.com>
2022-12-23 12:56:29 -0800

    Added home date feature

---
+++ src/home.js
@@ -0,0 +1,12 @@
+window.onload = setInterval(date,1000);
+
+function date(){
+    var d = new Date();
+    var date = d.getDate();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var monthArr = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+    month = monthArr[month];
+    document.getElementById("date").innerHTML=date+" "+month+", "+year;
+}
+
